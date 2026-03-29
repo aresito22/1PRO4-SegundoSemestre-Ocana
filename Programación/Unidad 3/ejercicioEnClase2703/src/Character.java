c4acb6e0d0cf98be0161e748bd052d74203a42bd
aresito22 <sipotracuenta@gmail.com>
2026-03-29 14:12:18 -0300

    update 29-03

---
+++ Programación/Unidad 3/ejercicioEnClase2703/src/Character.java
@@ -0,0 +1,92 @@
+public class Character {
+    private String name;
+    private int level;
+    private int healthPoints;
+    public boolean isBeaten;
+
+    public Character(String name) {
+        this.name = name;
+        level = 1;
+        healthPoints = 100;
+        isBeaten = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (!name.isEmpty()) {
+            this.name = name;
+        }
+        else {
+            System.out.println("Error: campo vacío.");
+        }
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        if (level >= 1) {
+            this.level = level;
+        }
+        else {
+            System.out.println("Error: nivel menor a 1.");
+        }
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        if (healthPoints >= 0 && healthPoints <= 100) {
+            this.healthPoints = healthPoints;
+        }
+        else {
+            System.out.println("Error: salud menor a 0 o mayor a 100.");
+        }
+    }
+
+    public void takeDamage(int damagePoints) {
+        if (damagePoints < healthPoints) {
+            healthPoints -= damagePoints;
+            System.out.println(damagePoints + " de daño! Salud actual: " + healthPoints);
+        }
+        else {
+            healthPoints = 0;
+            isBeaten = true;
+            System.out.println("Salud actual: 0 - " + name + "derrotado.");
+        }
+    }
+
+    public void heal(int healingPoints) {
+        if (isBeaten) {
+            if (healthPoints + healingPoints < 100) {
+                healthPoints += healingPoints;
+                System.out.println("Personaje curado. Salud actual: " + healthPoints);
+            }
+            else {
+                healthPoints = 100;
+                System.out.println("Personaje totalmente curado!");
+            }
+        }
+        else {
+            System.out.println("Error: no se puede curar a un personaje derrotado.");
+        }
+    }
+
+    public void showState() {
+        System.out.println("Nombre: " + name);
+        System.out.println("Nivel: " + level);
+        System.out.println("Salud actual: " + healthPoints);
+        if (isBeaten) {
+            System.out.println("Personaje derrotado.");
+        }
+        else {
+            System.out.println("Personaje vivo.");
+        }
+    }
+}
